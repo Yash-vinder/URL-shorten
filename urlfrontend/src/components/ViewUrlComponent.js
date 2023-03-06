306d3e41d3cec47a95e21b400f3e37e2d5c512d3
Yash-vinder <123yashvinder@gmail.com>
2023-03-06 13:46:56 +0530

    second messsgae

---
+++ urlfrontend/src/components/ViewUrlComponent.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+
+const ViewUrlComponent= () => {
+    const [urls, setUrls] = useState([]);
+
+    useEffect(() => {
+      const fetchUrlAndSetUrl = async () => {
+        const result = await axios.get("http://localhost:3333/all",{headers: {
+            'Referrer-Policy': 'same-origin'
+          }
+        });
+        setUrls(result.data);
+      };
+      fetchUrlAndSetUrl();
+    }, [urls]);
+
+  return (
+    <div>
+      <table className="table">
+        <thead className="table-dark">
+          <tr>
+            <th>Original Url</th>
+            <th>Short Url</th>
+            <th>Click Count</th>
+          </tr>
+        </thead>
+        <tbody>
+          {urls.map((url, idx) => (
+            <tr key={idx}>
+              <td>{url.origUrl}</td>
+              <td>
+                <a href={`${url.shortUrl}`}>{url.shortUrl}</a>
+              </td>
+              <td>{url.clicks}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ViewUrlComponent;
